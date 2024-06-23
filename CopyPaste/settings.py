e75f423729ec0f52374d532d9be004694a2f2d1f
Asir <asiradnan23@gmail.com>
2024-06-23 13:30:57 +0600

    Initialized

--- CopyPaste/settings.py
+++ CopyPaste/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["vercel.app","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app","127.0.0.1"]
 
 # Application definition
 
