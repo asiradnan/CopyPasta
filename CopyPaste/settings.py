54bc8993fcebf888a799c3370bbfa3425a60fd0c
Asir <asiradnan23@gmail.com>
2024-06-30 10:10:53 +0600

    Added fileuploads

--- CopyPaste/settings.py
+++ CopyPaste/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("secret_key")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [".vercel.app","127.0.0.1"]
 INSTALLED_APPS = [
     'django.contrib.admin',
