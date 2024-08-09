849eda6bc972095235a21fbb00299594fcf461c4
asiradnan <asiradnan23@gmail.com>
2024-08-09 22:54:23 +0600

    Fixed

--- CopyPaste/settings.py
+++ CopyPaste/settings.py
@@ -62,10 +62,8 @@ USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-] 
 STATIC_ROOT = os.path.join("staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
