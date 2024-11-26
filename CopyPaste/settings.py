a9b5790b41f8a74e5795bfbf75bff33c627462ab
asiradnan <asiradnan23@gmail.com>
2024-11-26 17:25:49 +0600

    Fixed serving media files

--- CopyPaste/settings.py
+++ CopyPaste/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("secret_key")
 DEBUG = False
-ALLOWED_HOSTS = ["copypasta.asiradnan.com","www.copypasta.asiradnan.com","*"]
+ALLOWED_HOSTS = ["copypasta.asiradnan.com","www.copypasta.asiradnan.com"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -62,4 +62,4 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join("staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join("mediafiles")
+MEDIA_ROOT = "/var/www/copypaste_mediafiles"
