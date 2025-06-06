c270890df57d23972b90b3e5deb1c740a4b32fe7
asiradnan <asiradnan23@gmail.com>
2025-06-06 10:08:25 +0600

    using cloudfront

--- CopyPaste/settings.py
+++ CopyPaste/settings.py
@@ -70,7 +70,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'copypasta-bucket'
 
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_CUSTOM_DOMAIN = "d2qwsjgkauqre9.cloudfront.net"
 AWS_S3_FILE_OVERWRITE = False
 
 STORAGES = {
